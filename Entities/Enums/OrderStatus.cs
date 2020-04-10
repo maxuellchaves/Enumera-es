a6f1979919a691d5d2b74eedee0a4690c4c0520a
Maxuell Chaves <maxuellchaves30@gmail.com>
2020-04-10 15:27:14 -0300

    criado a classe OrderStatus e iniciado os valores no programa inicial

---
+++ Entities/Enums/OrderStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerações.Entities.Enums
+{
+    enum OrderStatus : int
+
+    {
+        PendingPayment = 0,
+        Processing =1,
+        Shipped =2,
+        Delivered = 3
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Enumerações.Entities;
+using Enumerações.Entities.Enums;
 
 namespace Enumerações
 {
@@ -10,6 +12,25 @@ namespace Enumerações
     {
         static void Main(string[] args)
         {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+           
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus OS;
+            Enum.TryParse("Delivered", true, out OS);
+
+
+
+            Console.WriteLine(OS);
+            Console.WriteLine(txt);
         }
     }
 }
