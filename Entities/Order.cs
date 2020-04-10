943b2fa23244985211d25b1f24ec79a2be895fba
Maxuell Chaves <maxuellchaves30@gmail.com>
2020-04-10 14:24:19 -0300

    criado a classe order

---
+++ Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerações.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+
+    }
+}
